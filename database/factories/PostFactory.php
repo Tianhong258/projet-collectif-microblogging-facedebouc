0ad99e26199eec3879fa232a4004af974b44b873
MarieLeLamer <lelamer.marie17@gmail.com>
2024-04-10 14:27:59 +0200

    ajout clé étrangère et seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,22 +2,19 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
- */
 class PostFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Post::class;
+
+    public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'user_id' => \App\Models\User::factory(), // Créer un utilisateur associé à chaque post
         ];
     }
 }
